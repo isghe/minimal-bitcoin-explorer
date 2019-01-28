75a65c5ff9dc8b271bee5d0056e1db067c797814
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2019-01-28 11:00:36 +0100

    assert(info.changes === 1);

--- explore.js
+++ explore.js
@@ -20,46 +20,65 @@ const db = {
 			.run();
 	},
 	insertBlock: (index, hash) => {
-		return explore.db.prepare('insert into block(blockid, hash) values (?, ?)')
+		const info = explore.db.prepare('insert into block(blockid, hash) values (?, ?)')
 			.run(index, hash);
+		assert(info.changes === 1);
+		return info;
 	},
 	insertTransaction: (txid, block_ref) => {
-		return explore.db.prepare('insert into h_transaction(txid, block_ref) values (?, ?)')
+		const info = explore.db.prepare('insert into h_transaction(txid, block_ref) values (?, ?)')
 			.run(txid, block_ref);
+		assert(info.changes === 1);
+		return info;
 	},
 	insertUtxo: (transaction_ref, vout, value) => {
-		return explore.db.prepare('insert into utxo(transaction_ref, vout, value) values (?, ?, ?)')
+		const info = explore.db.prepare('insert into utxo(transaction_ref, vout, value) values (?, ?, ?)')
 			.run(transaction_ref, vout, value);
+		assert(info.changes === 1);
+		return info;
 	},
 	upsertSpkType: type => {
-		explore.db.prepare('insert into spk_type (description, counter) values (?, 1) ON CONFLICT(description) DO UPDATE SET counter = (select counter + 1 from spk_type where description = ?)')
+		const info = explore.db.prepare('insert into spk_type (description, counter) values (?, 1) ON CONFLICT(description) DO UPDATE SET counter = (select counter + 1 from spk_type where description = ?)')
 			.run(type, type);
+		assert(info.changes === 1);
+		return info;
 	},
 	upsertAddress: (text, hex_ref) => {
-		explore.db.prepare('insert into address(address, hex_ref, counter) values (?, (' + hex_ref + '),1) ON CONFLICT(address) DO UPDATE SET counter = (select counter + 1 from address where address = ?)')
+		const info = explore.db.prepare('insert into address(address, hex_ref, counter) values (?, (' + hex_ref + '),1) ON CONFLICT(address) DO UPDATE SET counter = (select counter + 1 from address where address = ?)')
 			.run(text, text);
+		assert(info.changes === 1);
+		return info;
 	},
 	upsertHex: (hex, spk_type_ref, satoshi) => {
-		explore.db.prepare('insert into hex(hex, spk_type_ref, counter, satoshi) values (?, (' + spk_type_ref + '),1, ?) ON CONFLICT(hex) DO UPDATE SET counter = (select counter + 1 from hex where hex = ?)')
+		const info = explore.db.prepare('insert into hex(hex, spk_type_ref, counter, satoshi) values (?, (' + spk_type_ref + '),1, ?) ON CONFLICT(hex) DO UPDATE SET counter = (select counter + 1 from hex where hex = ?)')
 			.run(hex, satoshi, hex);
+		assert(info.changes === 1);
+		return info;
 	},
 	insertUtxoHex: (utxo_ref, ref) => {
-		return explore.db.prepare('insert into utxo_hex(utxo_ref, hex_ref) values (?, (' + ref + '))')
+		const info = explore.db.prepare('insert into utxo_hex(utxo_ref, hex_ref) values (?, (' + ref + '))')
 			.run(utxo_ref);
+		assert(info.changes === 1);
+		return info;
 	},
-
 	updateHex: (hex, deltaSatoshi) => {
 		assert(hex);
-		explore.db.prepare('update hex set satoshi = (select satoshi + ? from hex where hex = ?) where hex = ?')
+		const info = explore.db.prepare('update hex set satoshi = (select satoshi + ? from hex where hex = ?) where hex = ?')
 			.run(deltaSatoshi, hex, hex);
+		assert(info.changes === 1);
+		return info;
 	},
 	selectVout: (txid, vout) => {
-		return explore.db.prepare('select id, hex, value from vv_utxo_hex where transaction_ref = (select id from h_transaction where txid = ?) and vout = ? and spent=0')
+		const ret = explore.db.prepare('select id, hex, value from vv_utxo_hex where transaction_ref = (select id from h_transaction where txid = ?) and vout = ? and spent=0')
 			.get(txid, vout);
+		assert(ret);
+		return ret;
 	},
 	updateUtxoSpent: id => {
-		explore.db.prepare('update utxo set spent = 1 where id = ?')
+		const info = explore.db.prepare('update utxo set spent = 1 where id = ?')
 			.run(id);
+		assert(info.changes === 1);
+		return info;
 	}
 };
 
