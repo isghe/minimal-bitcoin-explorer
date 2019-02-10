ff4c5f33b007f329fe2b57cdd5f2657e4237c1b9
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2019-02-10 17:59:40 +0100

    lib/util.js: sha256

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 'use strict';
 
+const assert = require('assert');
+const util = require('./lib/util.js');
+
 // 32.91*100000000 = 3290999999.9999995!!!
 console.log({value: 32.91 * 100000000, valueRound: Math.round(32.91 * 100000000)});
-
-const assert = require('assert');
 
 let hexUndefined;
 const hex = '';
@@ -12,3 +13,4 @@ assert(typeof hex !== 'undefined');
 assert(typeof hexUndefined === 'undefined');
 
 console.log(typeof undefined);
+console.log({testSha256: util.sha256('ciao') === 'b133a0c0e9bee3be20163d2ad31d6248db292aa6dcb1ee087a2aa50e0fc75ae2'});
