25696398f03ceb5e7790a104e3354ad971dfb19e
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2019-02-20 13:23:36 +0100

    db-engine/download-all/fake.js: db.block.extract

--- db-engine/download-all/fake.js
+++ db-engine/download-all/fake.js
@@ -1,7 +1,16 @@
 'use strict';
 
 const fake = () => {
-	return require('../common/fake');
+	const dbCommon = require('../common/fake');
+	const db = {
+		block: {
+			extract: block => {
+				return block;
+			}
+		}
+	};
+	db.block = Object.assign({}, dbCommon.block, db.block);
+	return Object.assign({}, dbCommon, db);
 };
 
 module.exports = fake();
